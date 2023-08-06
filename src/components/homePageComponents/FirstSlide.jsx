e23cd43ea6df61af639159fb006591010afab8aa
Andrey Dagaev <dandr212@gmail.com>
2023-08-06 21:26:37 +0300

    fix btn

--- src/components/homePageComponents/FirstSlide.jsx
+++ src/components/homePageComponents/FirstSlide.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import cl from './FirstSlide.module.css'
 import Typewriter from 'typewriter-effect';
 import Mainbutton from '../UI/Buttons/Mainbutton';
 import AnimatedBG from '../UI/AnimatedBackground/AnimatedBG';
+import { useNavigate } from 'react-router-dom';
+import { Context } from '../..';
+import { PROFILE_ROUTE, REGISTRATION_ROUTE } from '../../utils/consts';
 const FirstSlide = () => {
-
+    const {user} = useContext(Context)
+    const navigate = useNavigate()
     const phrases = [
         '<h2 className={cl.subHeader}>Проходи задания</h2>',
         '<h2 className={cl.subHeader}>Отслеживай прогресс</h2>',
         '<h2 className={cl.subHeader}>Зарабатывай достижения</h2>',
         '<h2 className={cl.subHeader}>Участвуй в турнирах</h2>',
     ];
-
+    const goToApp = () => {
+        if (user.isAuth) {
+            navigate(PROFILE_ROUTE)
+        } else {
+            navigate(REGISTRATION_ROUTE)
+        }
+    }
     return (
         <div className={cl.slideWrapper}>
             <AnimatedBG/>
@@ -33,7 +43,7 @@ const FirstSlide = () => {
                     }}
                 />
             </div>
-            <Mainbutton>НАЧАТЬ СЕЙЧАС</Mainbutton>
+            <Mainbutton onClick={() => {goToApp()}}>НАЧАТЬ СЕЙЧАС</Mainbutton>
         </div>
     );
 };
